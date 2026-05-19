410f80cb3742c4f85bfce69646deaec26c5337a0
Jefferson Silva <>
2026-05-19 13:12:01 +0100

    first commit

---
+++ Core/Core.swift
@@ -0,0 +1,9 @@
+//
+//  Core.swift
+//  Core
+//
+//  Created by Jefferson Carlos Souza da Silva on 16/05/2026.
+//
+
+import Foundation
+
---
+++ Core/Logger.swift
@@ -0,0 +1,13 @@
+//
+//  Logger.swift
+//  Core
+//
+//  Created by Jefferson Carlos Souza da Silva on 16/05/2026.
+//
+
+public class Logger {
+    
+    final class func log() {
+        print("ddd")
+    }
+}
