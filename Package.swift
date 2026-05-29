218e3865e9d6b8bb0d50ce04ab1a0ae85a3045c3
Jefferson Silva <>
2026-05-30 00:36:19 +0100

    Adding SPM

--- Package.swift
+++ Package.swift
@@ -1,24 +1,22 @@
 
-// swift-tools-version:5.10
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "JSCore",
     platforms: [
+        // Add support for all platforms starting from a specific version.
         .iOS(.v13)
     ],
     products: [
-        .library(
-            name: "JSCore",
-            targets: ["JSCore"]
-        )
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(name: "JSCore", targets: ["JSCore"])
     ],
-    dependencies: [],
     targets: [
-        .target(
-            name: "JSCore",
-            url: "https://github.com/JCarlos-Dev/Core/blob/main/JSCorePackage/JSCore.xcframework.zip",
-            checksum: "The checksum of the XCFramework inside the ZIP archive."
-        )
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "JSCore")
     ]
 )
